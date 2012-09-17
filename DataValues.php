561b4bb10f108a3e41ed3a6b27ec9d854a4b72aa
jeroendedauw <jeroendedauw@gmail.com>
2012-09-17 18:55:35 +0200

    Added common entrypoint

---
+++ DataValues.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Collective entry point for the extensions contained within the DataValues repository.
+ *
+ * @file
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+
+include __DIR__ . '/DataValues/DataValues.php';
+include __DIR__ . '/ValueParser/ValueParser.php';
+include __DIR__ . '/ValueValidator/ValueValidator.php';
+include __DIR__ . '/ValueFormatter/ValueFormatter.php';
+include __DIR__ . '/DataTypes/DataTypes.php';
