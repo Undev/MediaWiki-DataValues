7ce3a6026130c58d03baddb3a56dad05721ea065
jeroendedauw <jeroendedauw@gmail.com>
2012-09-18 12:14:00 +0200

    Work on entry point stuff and type factory

--- DataValues.php
+++ DataValues.php
@@ -3,6 +3,15 @@
 /**
  * Collective entry point for the extensions contained within the DataValues repository.
  *
+ * The entry point can be used by both MediaWiki and other PHP applications.
+ *
+ * In case of MediaWiki, the MEDIAWIKI constant needs to be defined. This
+ * will be the case when the entry point is included via LocalSettings.php,
+ * just like done with regular extensions.
+ *
+ * In case of another PHP application, you will need to set the DATAVALUES
+ * constant (this is required for security reasons).
+ *
  * @file
  *
  * @licence GNU GPL v2+
