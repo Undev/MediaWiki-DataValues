5bcf803910bfccc9df000ab8bed9d9b2e08d6152
jeroendedauw <jeroendedauw@gmail.com>
2013-05-07 13:46:46 +0200

    Remove pointless checks that obstruct library usage
    Change-Id: I1695b2f64c3aff2688f806c5fc20416fb688bd2f

--- ValueValidators/ValueValidators.php
+++ ValueValidators/ValueValidators.php
@@ -45,14 +45,6 @@
  * @ingroup Test
  */
 
-if ( !defined( 'DATAVALUES' ) && !defined( 'MEDIAWIKI' ) ) {
-	die( 'Not an entry point.' );
-}
-
-if ( !defined( 'DATAVALUES' ) ) {
-	define( 'DATAVALUES', true );
-}
-
 define( 'ValueValidators_VERSION', '0.1 alpha' );
 
 global $wgValueValidators;
