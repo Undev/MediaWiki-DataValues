009e846e5cf7dc55a66dfaa12aee16a7efa748c9
daniel <daniel.kinzler@wikimedia.de>
2013-06-20 21:31:54 +0200

    Allow Result objects to be merged
    Introducing Result::merge( $a, $b )
    
    Change-Id: Ie40b0bb5ffba444e2daedeaa137d7fbf946f5dd0

--- ValueValidators/includes/Result.php
+++ ValueValidators/includes/Result.php
@@ -65,6 +65,40 @@ class Result implements \Immutable {
 	}
 
 	/**
+	 * Returns a result that represents the combination of the two given results.
+	 * In particular, this means:
+	 *
+	 * If $a->getErrors() is empty and $a->isValid() is true, $b is returned.
+	 * If $b->getErrors() is empty and $b->isValid() is true, $a is returned.
+	 *
+	 * Otherwise, a new Result is constructed that contains
+	 * all errors from $a and $b, and is considered valid
+	 * if both $a and $b were valid.
+	 *
+	 * @since 0.1
+	 *
+	 * @param Result $a
+	 * @param Result $b
+	 *
+	 * @return Result
+	 */
+	public static function merge( Result $a, Result $b ) {
+		$aErrors = $a->getErrors();
+		$bErrors = $b->getErrors();
+
+		if ( $a->isValid() && empty( $aErrors ) ) {
+			return $b;
+		} elseif( $b->isValid() && empty( $bErrors ) ) {
+			return $a;
+		} else {
+			$errors = array_merge( $aErrors, $bErrors );
+			$valid = ( $a->isValid() && $b->isValid() );
+
+			return new Result( $valid, $errors );
+		}
+	}
+
+	/**
 	 * @since 0.1
 	 *
 	 * @param boolean $isValid
