faa771a1f25f36c87a9b8748e6a573cb0a7d81e2
Henning Snater <henning.snater@wikimedia.de>
2013-08-01 10:25:30 +0200

    ValueFormatter for Time DataValue
    (bug 48937)
    
    Change-Id: I0dd02909834ce8b428daea86e8bd2bf87ba9a085

--- ValueFormatters/ValueFormatters.php
+++ ValueFormatters/ValueFormatters.php
@@ -58,7 +58,8 @@ global $wgValueFormatters;
  * @deprecated since 0.1 This is a global registry that provides no control over object lifecycle
  */
 $wgValueFormatters = array(
-	'globecoordinate' => 'ValueFormatters\GeoCoordinateFormatter'
+	'globecoordinate' => 'ValueFormatters\GeoCoordinateFormatter',
+	'timevalue' => 'ValueFormatters\TimeFormatter',
 );
 
 spl_autoload_register( function ( $className ) {
