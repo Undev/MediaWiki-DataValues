88f7496b62af1c238bdf0a9fc69446e67e087322
Henning Snater <henning.snater@wikimedia.de>
2013-12-16 16:47:01 +0100

    Implemented basic front-end value formatters
    (bug 56261)
    This change introduces a NullFormatter as well as a formatter for the string data type to
    the front-end. In order to keep the change set size small, applying the formatters will be
    dealt with in a subsequent change set.
    
    Change-Id: I10481061ac0ee925cd70b5e321153ba433cd7c26

---
+++ DataValuesCommon/js/ValueFormatters.resources.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Definition of ResourceLoader modules of the ValueFormatters extension.
+ * When included this returns an array with all the modules introduced by ValueFormatters.
+ *
+ * @since 0.1
+ *
+ * @licence GNU GPL v2+
+ * @author H. Snater < mediawiki@snater.com >
+ *
+ * @codeCoverageIgnoreStart
+ */
+return call_user_func( function() {
+
+	$moduleTemplate = array(
+		'localBasePath' => __DIR__ . '/src/ValueFormatters',
+		'remoteExtPath' =>  'DataValues/DataValuesCommon/js/src/ValueFormatters',
+	);
+
+	return array(
+
+		'valueFormatters' => $moduleTemplate + array(
+			'scripts' => array(
+				'valueFormatters.js',
+			),
+		),
+
+		'valueFormatters.ValueFormatter' => $moduleTemplate + array(
+			'scripts' => array(
+				'formatters/ValueFormatter.js',
+			),
+			'dependencies' => array(
+				'valueFormatters',
+				'valueFormatters.util',
+			),
+		),
+
+		'valueFormatters.formatters' => $moduleTemplate + array(
+			'scripts' => array(
+				'formatters/NullFormatter.js',
+				'formatters/StringFormatter.js',
+			),
+			'dependencies' => array(
+				'dataValues.values',
+				'valueFormatters.ValueFormatter',
+			),
+		),
+
+		'valueFormatters.util' => $moduleTemplate + array(
+				'scripts' => array(
+					'valueFormatters.util.js',
+				),
+				'dependencies' => array(
+					'dataValues.util',
+					'valueFormatters',
+				),
+			),
+
+	);
+
+} );
